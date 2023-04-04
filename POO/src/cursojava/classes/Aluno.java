defdd165c19cf1e5093201ee56630d74e9762462
Ronaldo2017 <{ID}+{username}@users.noreply.github.com>
2023-04-04 11:50:25 -0300

    Começando POO, criado primeira classe

---
+++ POO/src/cursojava/classes/Aluno.java
@@ -0,0 +1,26 @@
+package cursojava.classes;
+
+public class Aluno {
+	
+	//atributos da classe
+	public String nome;
+	public int idade;
+	public String dataNasc;
+	public String rg;
+	public String cpf;
+	public String nomeMae;
+	public String nomePai;
+	public String dataMatricula;
+	public String nomeEscola;
+	public String serieMatriculado;
+	
+	public Aluno() {
+		//cria os dados na memoria = Padrao
+	}
+	
+	public Aluno(String nomePadrao) {
+		nome = nomePadrao;
+	}
+	
+
+}
---
+++ POO/src/cursojava/executavel/PrimeiraClasseJava.java
@@ -0,0 +1,23 @@
+package cursojava.executavel;
+
+import cursojava.classes.Aluno;
+
+public class PrimeiraClasseJava {
+
+	public static void main(String[] args) {
+		//criando o objeto na memoria
+		//new Aluno(); = é uma instancia
+		//aluno1 é uma referencia para o objeto Aluno
+		Aluno aluno1 = new Aluno();
+		
+		Aluno aluno2 = new Aluno();
+		
+		Aluno aluno3 = new Aluno("Mario");
+		
+		
+		
+		
+
+	}
+
+}
