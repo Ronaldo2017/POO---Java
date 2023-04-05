84f25c247a36e9bff1cba680f0c335aa9b7cb352
Ronaldo2017 <{ID}+{username}@users.noreply.github.com>
2023-04-05 08:50:05 -0300

    Incluido get e set, equal e hashcode, entrada de dados

--- POO/src/cursojava/classes/Aluno.java
+++ POO/src/cursojava/classes/Aluno.java
@@ -1,26 +1,201 @@
 package cursojava.classes;
 
 public class Aluno {
-	
-	//atributos da classe
-	public String nome;
-	public int idade;
-	public String dataNasc;
-	public String rg;
-	public String cpf;
-	public String nomeMae;
-	public String nomePai;
-	public String dataMatricula;
-	public String nomeEscola;
-	public String serieMatriculado;
-	
+
+	// atributos da classe
+	private String nome;
+	private int idade;
+	private String dataNasc;
+	private String rg;
+	private String cpf;
+	private String nomeMae;
+	private String nomePai;
+	private String dataMatricula;
+	private String nomeEscola;
+	private String serieMatriculado;
+	private double n1;
+	private double n2;
+	private double n3;
+	private double n4;
+
+	/*
+	 * get é para resagater ou obter o valor do atributo
+	 * 
+	 * set é para adicionar ou receber dados para os atributos
+	 */
+
 	public Aluno() {
-		//cria os dados na memoria = Padrao
+		// cria os dados na memoria = Padrao
 	}
-	
+
 	public Aluno(String nomePadrao) {
 		nome = nomePadrao;
 	}
-	
+
+	/* recebe dados */
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public int getIdade() {
+		return idade;
+	}
+
+	public void setIdade(int idade) {
+		this.idade = idade;
+	}
+
+	public String getDataNasc() {
+		return dataNasc;
+	}
+
+	public void setDataNasc(String dataNasc) {
+		this.dataNasc = dataNasc;
+	}
+
+	public String getRg() {
+		return rg;
+	}
+
+	public void setRg(String rg) {
+		this.rg = rg;
+	}
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+	public String getNomeMae() {
+		return nomeMae;
+	}
+
+	public void setNomeMae(String nomeMae) {
+		this.nomeMae = nomeMae;
+	}
+
+	public String getNomePai() {
+		return nomePai;
+	}
+
+	public void setNomePai(String nomePai) {
+		this.nomePai = nomePai;
+	}
+
+	public String getDataMatricula() {
+		return dataMatricula;
+	}
+
+	public void setDataMatricula(String dataMatricula) {
+		this.dataMatricula = dataMatricula;
+	}
+
+	public String getNomeEscola() {
+		return nomeEscola;
+	}
+
+	public void setNomeEscola(String nomeEscola) {
+		this.nomeEscola = nomeEscola;
+	}
+
+	public String getSerieMatriculado() {
+		return serieMatriculado;
+	}
+
+	public void setSerieMatriculado(String serieMatriculado) {
+		this.serieMatriculado = serieMatriculado;
+	}
+
+	public double getN1() {
+		return n1;
+	}
+
+	public void setN1(double n1) {
+		this.n1 = n1;
+	}
+
+	public double getN2() {
+		return n2;
+	}
+
+	public void setN2(double n2) {
+		this.n2 = n2;
+	}
+
+	public double getN3() {
+		return n3;
+	}
+
+	public void setN3(double n3) {
+		this.n3 = n3;
+	}
+
+	public double getN4() {
+		return n4;
+	}
+
+	public void setN4(double n4) {
+		this.n4 = n4;
+	}
+
+	public double getMediaNota() {
+		return (n1 + n2 + n3 + n4) / 4;
+	}
+
+	public boolean getAlunoAprovado() {
+		double media = this.getMediaNota();
+		if (media >= 70) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	/* representa os dados do objeto toString */
+	@Override
+	public String toString() {
+		return "Aluno [nome=" + nome + ", idade=" + idade + ", dataNasc=" + dataNasc + ", rg=" + rg + ", cpf=" + cpf
+				+ ", nomeMae=" + nomeMae + ", nomePai=" + nomePai + ", dataMatricula=" + dataMatricula + ", nomeEscola="
+				+ nomeEscola + ", serieMatriculado=" + serieMatriculado + ", n1=" + n1 + ", n2=" + n2 + ", n3=" + n3
+				+ ", n4=" + n4 + "]";
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((cpf == null) ? 0 : cpf.hashCode());
+		result = prime * result + ((nome == null) ? 0 : nome.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Aluno other = (Aluno) obj;
+		if (cpf == null) {
+			if (other.cpf != null)
+				return false;
+		} else if (!cpf.equals(other.cpf))
+			return false;
+		if (nome == null) {
+			if (other.nome != null)
+				return false;
+		} else if (!nome.equals(other.nome))
+			return false;
+		return true;
+	}
 
 }
