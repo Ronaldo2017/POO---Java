77d11f8102954b2d4ede310e8b52840a842950e7
Ronaldo2017 <{Ronaldo2017}@users.noreply.github.com>
2023-04-08 09:51:45 -0300

    percorrendo lista de aluno e disciplina, troca de objeto

---
+++ POO/src/cursojava/classes/CodigosNaoUtilizados.java
@@ -0,0 +1,28 @@
+package cursojava.classes;
+
+public class CodigosNaoUtilizados {
+
+//	for (Aluno aluno : alunos) {
+//
+//		if (aluno.getNome().equalsIgnoreCase("juca")) {
+//			alunos.remove(aluno);
+//			break;
+//		} else {
+//			System.out.println(aluno.toString());// descrição do objeto na memoria
+//			System.out.println("Media: " + aluno.getMediaNota());
+//			System.out.println("Resultado: " + (aluno.getAlunoAprovado2()));
+//			System.out.println("------------------------------------------");
+//		}
+//	}
+//	
+//	//percorre e mostra as disciplinas do aluno na lista
+//	for (Aluno aluno : alunos) {
+//		System.out.println("Aluno que sobraram na lista");
+//		System.out.println(aluno.getNome());
+//		System.out.println("Suas materias são: ");
+//		//percorre as disciplinas que o aluno tem
+//		for (Disciplina disciplina : aluno.getDisciplinas()) {
+//			System.out.println(disciplina.getDisciplina());
+//		}
+//	}
+}
--- POO/src/cursojava/executavel/PrimeiraClasseJava.java
+++ POO/src/cursojava/executavel/PrimeiraClasseJava.java
@@ -1,5 +1,8 @@
 package cursojava.executavel;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.swing.JOptionPane;
 
 import cursojava.classes.Aluno;
@@ -11,9 +14,14 @@ public class PrimeiraClasseJava {
 		// criando o objeto na memoria
 		// new Aluno(); = é uma instancia
 		// aluno1 é uma referencia para o objeto Aluno
-		
-		String nome = JOptionPane.showInputDialog("Qual o nome do aluno");
-		String idade = JOptionPane.showInputDialog("Qual a idade do aluno");
+
+		// lista de aluno
+		List<Aluno> alunos = new ArrayList<Aluno>();
+
+		for (int qtd = 1; qtd <= 2; qtd++) {
+
+			String nome = JOptionPane.showInputDialog("Qual o nome do aluno " + qtd);
+//			String idade = JOptionPane.showInputDialog("Qual a idade do aluno");
 //		String rg = JOptionPane.showInputDialog("Qual o RG do aluno");
 //		String cpf = JOptionPane.showInputDialog("Qual o CPF do aluno");
 //		String mae = JOptionPane.showInputDialog("Qual Nome da Mãe do aluno");
@@ -21,12 +29,12 @@ public class PrimeiraClasseJava {
 //		String matricula = JOptionPane.showInputDialog("Qual data da matrícula do aluno");
 //		String nascimento = JOptionPane.showInputDialog("Qual Data de nascimento do aluno");
 //		String serie = JOptionPane.showInputDialog("Qual a serie matriculado do aluno");
-		
-		//instanciando o objeto
-		Aluno aluno1 = new Aluno();
-		
-		aluno1.setNome(nome);
-		aluno1.setIdade(Integer.valueOf(idade));
+
+			// instanciando o objeto
+			Aluno aluno1 = new Aluno();
+
+			aluno1.setNome(nome);
+//			aluno1.setIdade(Integer.valueOf(idade));
 //		aluno1.setDataNasc(nascimento);
 //		aluno1.setDataMatricula(matricula);
 //		aluno1.setRg(rg);
@@ -34,68 +42,72 @@ public class PrimeiraClasseJava {
 //		aluno1.setNomeMae(mae);
 //		aluno1.setNomePai(pai);
 //		aluno1.setSerieMatriculado(serie);
-		
-		//dinamica das diciplinas
-		for(int pos = 1; pos <= 2;pos++) {
-			String nomeDisciplina = JOptionPane.showInputDialog("Nome da Disciplina " + pos);
-			String notaDisciplina = JOptionPane.showInputDialog("Nota da Disciplina " + pos);
-			
-			Disciplina disciplina = new Disciplina();
-			disciplina.setDisciplina(nomeDisciplina);
-			disciplina.setN(Double.valueOf(notaDisciplina));
-			
-			aluno1.getDisciplinas().add(disciplina);
+
+			// dinamica das diciplinas
+			for (int pos = 1; pos <= 2; pos++) {
+				String nomeDisciplina = JOptionPane.showInputDialog("Nome da Disciplina " + pos);
+				String notaDisciplina = JOptionPane.showInputDialog("Nota da Disciplina " + pos);
+
+				Disciplina disciplina = new Disciplina();
+				disciplina.setDisciplina(nomeDisciplina);
+				disciplina.setN(Double.valueOf(notaDisciplina));
+
+				aluno1.getDisciplinas().add(disciplina);
+			}
+
+			// removendo disciplina
+			int escolha = JOptionPane.showConfirmDialog(null, "Deseja remover alguma disciplina?");
+			if (escolha == 0) {// opcao sim = 0
+				int continuarRemover = 0;
+				int posicao = 1;
+				while (continuarRemover == 0) {
+					String removerDisciplina = JOptionPane.showInputDialog("Qual disciplina 1, 2, 3 ou 4 ?");
+					aluno1.getDisciplinas().remove(Integer.valueOf(removerDisciplina).intValue() - posicao);
+					posicao++;
+
+					continuarRemover = JOptionPane.showConfirmDialog(null, "Continuar a remover ?");
+				}
+
+			}
+
+			// adiciona o aluno na lista
+			alunos.add(aluno1);
 		}
-		
-		//removendo disciplina
-		int escolha = JOptionPane.showConfirmDialog(null, "Deseja remover alguma disciplina?");
-		if(escolha == 0) {//opcao sim = 0
-			int continuarRemover = 0;
-			int posicao = 1;
-			while(continuarRemover == 0) {
-				String removerDisciplina = JOptionPane.showInputDialog("Qual disciplina 1, 2, 3 ou 4 ?");
-				aluno1.getDisciplinas().remove(Integer.valueOf(removerDisciplina).intValue() - posicao);
-				posicao ++;
-				continuarRemover = JOptionPane.showConfirmDialog(null, "Continuar a remover ?");
+
+		// percorrendo a lista de aluno pela posicao
+		for (int pos = 0; pos < alunos.size(); pos++) {
+			Aluno aluno = alunos.get(pos);
+			
+			if(aluno.getNome().equalsIgnoreCase("juca")) {
+				Aluno trocar = new Aluno();
+				trocar.setNome("Pedro");
+				
+				Disciplina disciplina = new Disciplina();
+				disciplina.setDisciplina("Jquery");
+				disciplina.setN(88);
+				
+				//adiciona o objeto trocado na lista
+				trocar.getDisciplinas().add(disciplina);
+				
+				alunos.set(pos, trocar);
+				aluno = alunos.get(pos);
+				
 			}
 			
+			System.out.println("Aluno: " + aluno.getNome());
+			System.out.println("Média: " + aluno.getMediaNota());
+			System.out.println("Resultado: " + aluno.getAlunoAprovado2());
+			System.out.println("------------------------------------------");
+
+			// percorrendo a lista de disciplina pela posicao
+			for (int posd = 0; posd < aluno.getDisciplinas().size(); posd++) {
+				Disciplina disc = aluno.getDisciplinas().get(posd);
+				System.out.println("Materia: " + disc.getDisciplina() + " Nota: " + disc.getN());
+				System.out.println("------------------------------------------");
+			}
+
 		}
-
-		//Aluno aluno = new Aluno();
-		System.out.println(aluno1.toString());//descrição do objeto na memoria
-		System.out.println("Media: " + aluno1.getMediaNota());
-//		System.out.println("Resultado: " + (aluno1.getAlunoAprovado() ? "Aprovado" : "Reprovado"));
-		System.out.println("Resultado: " + (aluno1.getAlunoAprovado2()));
-
 
 	}
 
 }
-
-
-
-
-//System.out.println(aluno1.getNome());
-//System.out.println(aluno1.getIdade());
-//System.out.println(aluno1.getDataMatricula());
-//System.out.println(aluno1.getCpf());
-//System.out.println(aluno1.getMediaNota());
-//System.out.println("Resultado:" + (aluno1.getAlunoAprovado() ? "Aprovado" : "Reprovado"));
-
-
-
-//Equals e hashcode = deferenciar e comparar objeto
-//Aluno aluno1= new Aluno();
-//aluno1.setNome("Pedro");
-//aluno1.setCpf("54687645");
-//
-//Aluno aluno2 = new Aluno();
-//aluno2.setNome("Pedro");
-//aluno2.setCpf("54687645");
-//
-//if(aluno1.equals(aluno2)) {
-//	System.out.println("São iguais");
-//}else {
-//	System.out.println("Não são iguais");
-//}
-
